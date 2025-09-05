5519a38d04c86fc3a0a9e3ba59ad61409f46a851
legitbox <materixasqwerty@gmail.com>
2025-09-05 23:41:38 +0300

    added debugging and better error handling

--- main.ts
+++ main.ts
@@ -2,32 +2,14 @@ import { run } from './runtime';
 
 let code: string =
 `
-SET 100000 >> limit
-SET 2 >> current
+SET 10 >> num1
+SET 20 >> num2
+set 0 >> result
 
-POINT outer_loop
-SET 1 >> is_prime
-SET 2 >> divisor
-
-POINT inner_loop
-MATH current % divisor >> remainder
-
-IF remainder == 0 >> not_prime
-MATH divisor + 1 >> divisor
-IF divisor < current >> inner_loop
-
-IF is_prime == 1 >> print_prime
-POINT not_prime
-JUMP next_number
-
-POINT print_prime
-PRINT current
-
-POINT next_number
-MATH current + 1 >> current
-IF current <= limit >> outer_loop
-
-PRINT "Prime calculation complete!"`
+MATH num1 + num2 >> result
+if result > 5 >> thingy
+POINT thingy
+PRINT result`
 
 console.time('runtime')
 run(code);
--- runtime.ts
+++ runtime.ts
@@ -1,39 +1,57 @@
 let variableMemory: Map<string, number> = new Map();
 let jumpPoints: Map<string, number> = new Map();
 
-function variableCheck(variableName: string): number {
+enum OpCode {
+  SET,
+  MATH,
+  PRINT,
+  IF,
+  JUMP,
+  POINT
+}
+
+function variableCheck(variableName: string, counter: number): number {
   if (variableMemory.has(variableName)) {
     const variable = variableMemory.get(variableName)
     if (variable != undefined) {
       return variable;
     } else {
+      throw new Error("variable " + variableName + " is corrupt or got lost in memory!!! at line " + counter);
       return 0;
     }
   } else {
-    return parseFloat(variableName);
+    let number = 0;
+    try {
+      number = Number(variableName);
+    } catch (error) {
+      throw new Error("variable " + variableName + " is not a number at line " + counter);
+      return 0;
+    }
+    return number;
   }
 }
 
-function jumpPointCheck(jumpPointName: string): [boolean, number] {
+function jumpPointCheck(jumpPointName: string, counter: number): [boolean, number] {
   if (jumpPoints.has(jumpPointName)) {
     const jumpPoint = jumpPoints.get(jumpPointName)
     if (jumpPoint != undefined) {
       return [true, jumpPoint];
     } else {
-      console.error("jump point " + jumpPointName + " is not defined");
+      throw new Error("jump point " + jumpPointName + " is not defined at line " + counter);
       return [false, 0];
     }
   } else {
-    console.error("jump point " + jumpPointName + " is not defined");
+    throw new Error("jump point " + jumpPointName + " is not defined at line " + counter);
     return [false, 0];
   }
 }
 
 export function run(code: string) {
   let lines = code.split("\n");
-  // collect jump jumpPoints
+
+  // collect jump jumpPointsnumber
   for (let counter = 0; counter < lines.length; counter++) {
-    let line = lines[counter].trim();
+    let line = lines[counter];
     if (line === '' || line.startsWith('//')) {
       continue;
     }
@@ -46,7 +64,7 @@ export function run(code: string) {
   
   // Parse and run the code
   codeloop: for (let counter = 0; counter < lines.length; counter++) {
-    let line = lines[counter].trim();
+    let line = lines[counter];
     if (line === '' || line.startsWith('//')) {
       continue;
     }
@@ -54,7 +72,7 @@ export function run(code: string) {
     let operation = tokens[0].toUpperCase();
     switch (operation) {
       case "SET": {
-        variableMemory.set(tokens[3], parseFloat(tokens[1]));
+        variableMemory.set(tokens[3], Number(tokens[1]));
         break;
       }
       case "PRINT": {
@@ -62,65 +80,115 @@ export function run(code: string) {
           let text = line.substring(line.indexOf(" ") + 1);
           console.log(text.replaceAll(`"`, ""));
         } else {
-          console.log(variableCheck(tokens[1]));
+          const printVar = variableCheck(tokens[1], counter)
+          if (Number.isNaN(printVar)){
+            throw new Error("Invalid print operation at line " + counter);
+          } else {
+            console.log(printVar);
+          }
         }
         break;
       }
       case "MATH": {
         switch (tokens[2]) {
           case "+": {
-            let argument1 = variableCheck(tokens[1]);
-            let argument2 = variableCheck(tokens[3]);
-            variableMemory.set(tokens[5], argument1 + argument2);
+            const argument1 = variableCheck(tokens[1], counter);
+            const argument2 = variableCheck(tokens[3], counter);
+            const mathOp = argument1 + argument2;
+            if (Number.isNaN(mathOp)) {
+              throw new Error("Invalid operation at line " + counter);
+            } else {
+              variableMemory.set(tokens[5], argument1 + argument2);
+            }
             break;
           }
           case "-": {
-            let argument1 = variableCheck(tokens[1]);
-            let argument2 = variableCheck(tokens[3]);
-            variableMemory.set(tokens[5], argument1 - argument2);
+            const argument1 = variableCheck(tokens[1], counter);
+            const argument2 = variableCheck(tokens[3], counter);
+            const mathOp = argument1 - argument2;
+            if (Number.isNaN(mathOp)) {
+              throw new Error("Invalid operation at line " + counter);
+            } else {
+              variableMemory.set(tokens[5], mathOp);
+            }
             break;
           }
           case "*": {
-            let argument1 = variableCheck(tokens[1]);
-            let argument2 = variableCheck(tokens[3]);
-            variableMemory.set(tokens[5], argument1 * argument2);
+            const argument1 = variableCheck(tokens[1], counter);
+            const argument2 = variableCheck(tokens[3], counter);
+            const mathOp = argument1 * argument2;
+            if (Number.isNaN(mathOp)) {
+              throw new Error("Invalid operation at line " + counter);
+            } else {
+              variableMemory.set(tokens[5], mathOp);
+            }
             break;
           }
           case "/": {
-            let argument1 = variableCheck(tokens[1]);
-            let argument2 = variableCheck(tokens[3]);
-            variableMemory.set(tokens[5], argument1 / argument2);
+            const argument1 = variableCheck(tokens[1], counter);
+            const argument2 = variableCheck(tokens[3], counter);
+            const mathOp = argument1 / argument2;
+            if (Number.isNaN(mathOp)) {
+              throw new Error("Invalid operation at line " + counter);
+            } else {
+              variableMemory.set(tokens[5], mathOp);
+            }
             break;
           }
           case "%": {
-            let argument1 = variableCheck(tokens[1]);
-            let argument2 = variableCheck(tokens[3]);
-            variableMemory.set(tokens[5], argument1 % argument2);
+            const argument1 = variableCheck(tokens[1], counter);
+            const argument2 = variableCheck(tokens[3], counter);
+            const mathOp = argument1 % argument2;
+            if (Number.isNaN(mathOp)) {
+              throw new Error("Invalid operation at line " + counter);
+            } else {
+              variableMemory.set(tokens[5], mathOp);
+            }
             break;
           }
           case "**": {
-            let argument1 = variableCheck(tokens[1]);
-            let argument2 = variableCheck(tokens[3]);
-            variableMemory.set(tokens[5], Math.pow(argument1, argument2));
+            const argument1 = variableCheck(tokens[1], counter);
+            const argument2 = variableCheck(tokens[3], counter);
+            const mathOp = Math.pow(argument1, argument2);
+            if (Number.isNaN(mathOp)) {
+              throw new Error("Invalid operation at line " + counter);
+            } else {
+              variableMemory.set(tokens[5], mathOp);
+            }
             break;
           }
           case "sqrt": {
-            let argument1 = variableCheck(tokens[1]);
-            variableMemory.set(tokens[4], Math.sqrt(argument1));
+            const argument1 = variableCheck(tokens[1], counter);
+            const mathOp = Math.sqrt(argument1);
+            if (Number.isNaN(mathOp)) {
+              throw new Error("Invalid operation at line " + counter);
+            } else {
+              variableMemory.set(tokens[4], mathOp);
+            }
             break;
           }
           case "log": {
-            let argument1 = variableCheck(tokens[1]);
-            variableMemory.set(tokens[4], Math.log(argument1));
+            const argument1 = variableCheck(tokens[1], counter);
+            const mathOp = Math.log(argument1);
+            if (Number.isNaN(mathOp)) {
+              throw new Error("Invalid operation at line " + counter);
+            } else {
+              variableMemory.set(tokens[4], mathOp);
+            }
             break;
           }
           case "sin": {
-            let argument1 = variableCheck(tokens[1]);
-            variableMemory.set(tokens[5], Math.sin(argument1));
+            const argument1 = variableCheck(tokens[1], counter);
+            const mathOp = Math.sin(argument1);
+            if (Number.isNaN(mathOp)) {
+              throw new Error("Invalid operation at line " + counter);
+            } else {
+              variableMemory.set(tokens[5], mathOp);
+            }
             break;
           }
           default: {
-            console.error("invalid math operator!");
+            throw new Error("invalid math operator!");
             break codeloop;
           }
         }
@@ -130,73 +198,91 @@ export function run(code: string) {
         // IF result > 5 >> imdone
         switch (tokens[2]) {
           case ">": {
-            let argument1 = variableCheck(tokens[1]);
-            let argument2 = variableCheck(tokens[3]);
-            if (argument1 > argument2) {
-              const jumpPoint = jumpPointCheck(tokens[5])
-                if (jumpPoint[0] === true) {
-                  counter = jumpPoint[1];
-                }
+            const argument1 = variableCheck(tokens[1], counter);
+            const argument2 = variableCheck(tokens[3], counter);
+            const check = argument1 > argument2;
+            if (Number.isNaN(argument1) || Number.isNaN(argument2)) {
+              throw new Error("invalid comparison operator at line " + counter);
+            } else {
+              const jumpPoint = jumpPointCheck(tokens[5], counter);
+              if (jumpPoint[0] === true) {
+                counter = jumpPoint[1];
+              }
             }
             break;
           }
           case "<": {
-            let argument1 = variableCheck(tokens[1]);
-            let argument2 = variableCheck(tokens[3]);
-            if (argument1 < argument2) {
-              const jumpPoint = jumpPointCheck(tokens[5])
-                if (jumpPoint[0] === true) {
-                  counter = jumpPoint[1];
-                }
+            const argument1 = variableCheck(tokens[1], counter);
+            const argument2 = variableCheck(tokens[3], counter);
+            const check = argument1 < argument2;
+            if (Number.isNaN(argument1) || Number.isNaN(argument2)) {
+              throw new Error("invalid comparison operator at line " + counter);
+            } else {
+              const jumpPoint = jumpPointCheck(tokens[5], counter);
+              if (jumpPoint[0] === true) {
+                counter = jumpPoint[1];
+              }
             }
             break;
           }
           case "==": {
-            let argument1 = variableCheck(tokens[1]);
-            let argument2 = variableCheck(tokens[3]);
-            if (argument1 == argument2) {
-              const jumpPoint = jumpPointCheck(tokens[5])
-                if (jumpPoint[0] === true) {
-                  counter = jumpPoint[1];
-                }
+            const argument1 = variableCheck(tokens[1], counter);
+            const argument2 = variableCheck(tokens[3], counter);
+            const check = argument1 == argument2;
+            if (Number.isNaN(argument1) || Number.isNaN(argument2)) {
+              throw new Error("invalid comparison operator at line " + counter);
+            } else {
+              const jumpPoint = jumpPointCheck(tokens[5], counter);
+              if (jumpPoint[0] === true) {
+                counter = jumpPoint[1];
+              }
             }
             break;
           }
           case "!=": {
-            let argument1 = variableCheck(tokens[1]);
-            let argument2 = variableCheck(tokens[3]);
-            if (argument1 != argument2) {
-              const jumpPoint = jumpPointCheck(tokens[5])
-                if (jumpPoint[0] === true) {
-                  counter = jumpPoint[1];
-                }
+            const argument1 = variableCheck(tokens[1], counter);
+            const argument2 = variableCheck(tokens[3], counter);
+            const check = argument1 != argument2;
+            if (Number.isNaN(argument1) || Number.isNaN(argument2)) {
+              throw new Error("invalid comparison operator at line " + counter);
+            } else {
+              const jumpPoint = jumpPointCheck(tokens[5], counter);
+              if (jumpPoint[0] === true) {
+                counter = jumpPoint[1];
+              }
             }
             break;
           }
           case ">=": {
-            let argument1 = variableCheck(tokens[1]);
-            let argument2 = variableCheck(tokens[3]);
-            if (argument1 >= argument2) {
-              const jumpPoint = jumpPointCheck(tokens[5])
-                if (jumpPoint[0] === true) {
-                  counter = jumpPoint[1];
-                }
+            const argument1 = variableCheck(tokens[1], counter);
+            const argument2 = variableCheck(tokens[3], counter);
+            const check = argument1 >= argument2;
+            if (Number.isNaN(argument1) || Number.isNaN(argument2)) {
+              throw new Error("invalid comparison operator at line " + counter);
+            } else {
+              const jumpPoint = jumpPointCheck(tokens[5], counter);
+              if (jumpPoint[0] === true) {
+                counter = jumpPoint[1];
+              }
             }
             break;
           }
           case "<=": {
-            let argument1 = variableCheck(tokens[1]);
-            let argument2 = variableCheck(tokens[3]);
-            if (argument1 <= argument2) {
-              const jumpPoint = jumpPointCheck(tokens[5])
-                if (jumpPoint[0] === true) {
-                  counter = jumpPoint[1];
-                }
+            const argument1 = variableCheck(tokens[1], counter);
+            const argument2 = variableCheck(tokens[3], counter);
+            const check = argument1 <= argument2;
+            if (Number.isNaN(argument1) || Number.isNaN(argument2)) {
+              throw new Error("invalid comparison operator at line " + counter);
+            } else {
+              const jumpPoint = jumpPointCheck(tokens[5], counter);
+              if (jumpPoint[0] === true) {
+                counter = jumpPoint[1];
+              }
             }
             break;
           }
           default: {
-            console.error("invalid comparison operator!");
+            throw new Error("invalid comparison operator at line " + counter);
             break codeloop;
           }
         }
@@ -209,7 +295,7 @@ export function run(code: string) {
         break codeloop;
       }
         case "JUMP": {
-            const jumpPoint = jumpPointCheck(tokens[1])
+            const jumpPoint = jumpPointCheck(tokens[1], counter)
             if (jumpPoint[0] === true) {
                 counter = jumpPoint[1];
             }
